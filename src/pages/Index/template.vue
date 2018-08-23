ff714bf4ed6200b78deaaf49fadf4c24737b44e3
adashuai5 <ad873606339@gmail.com>
2018-08-23 21:33:43 +0800

    引入 ElementUI 组件

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="index">
     <p>首页</p>
+    <div>
+    <el-button @click="onClick1">默认按钮</el-button>
+    <el-button @click="onClick2">alert</el-button>
   </div>
+  </div>
+  
 </template>
 
 <script src="./template.js">
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less" src="./template.less">
-
 </style>
