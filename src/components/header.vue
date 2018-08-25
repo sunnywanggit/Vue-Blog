11002bac04e8b66229ceba4e6e8fdaf531b96d59
adashuai5 <ad873606339@gmail.com>
2018-08-25 16:00:45 +0800

    建立项目结构

---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>LET'S SHARE</h1>
+    <span>精品博客汇聚</span>
+    <div class="btns">
+      <el-button>立即登录</el-button>
+      <el-button>注册账号</el-button>
+    </div>
+  </div>
+</template>
+
