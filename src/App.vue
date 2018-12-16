f318fe96ea07506556bd8f5b68365e5d0d9a5b9b
adashuai5 <ad873606339@gmail.com>
2018-12-16 22:02:22 +0800

    移动端适配初步优化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-     <!-- Header 和 Footer 不要用小写是为了与原生 html 区别开来 -->
+    <!-- Header 和 Footer 不要用小写是为了与原生 html 区别开来 -->
     <Header id="header"></Header>
     <main id="main">
       <router-view/>
@@ -10,57 +10,54 @@
 </template>
 
 <script>
-import Header from "@/components/header.vue";
-import Footer from "@/components/footer.vue";
+  import Header from "@/components/header.vue";
+  import Footer from "@/components/footer.vue";
 
-export default {
-  name: "App",
-  components: {
-    // ES6 语法 相当于 Header：Header
-    Header,
-    Footer
-  }
-};
+  export default {
+    name: "App",
+    components: {
+      // ES6 语法 相当于 Header：Header
+      Header,
+      Footer
+    }
+  };
 </script>
 
 <style lang="less">
-@import "./assets/common.less";
+  @import "./assets/common.less";
 
-#app {
-  display: grid;
-  grid-template-columns: 12% auto 12%;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas:
-    "header header header"
-    ". main ."
-    "footer footer footer";
-
-  #header {
-    grid-area: header;
-    padding-left: 12%;
-    padding-right: 12%;
-  }
-
-  #main {
-    grid-area: main;
-  }
-
-  #footer {
-    grid-area: footer;
-    padding-left: 12%;
-    padding-right: 12%;
-  }
-}
-
-@media (max-width: 768px) {
   #app {
-    grid-template-columns: 10px auto 10px;
+    display: grid;
+    grid-template-columns: 12% auto 12%;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas: "header header header" ". main ." "footer footer footer";
 
-    #header,
+    #header {
+      grid-area: header;
+      padding-left: 12%;
+      padding-right: 12%;
+    }
+
+    #main {
+      grid-area: main;
+    }
+
     #footer {
-      padding-left: 10px;
-      padding-right: 10px;
+      grid-area: footer;
+      padding-left: 12%;
+      padding-right: 12%;
     }
   }
-}
+
+  @media (max-width: 768px) {
+    #app {
+      grid-template-columns: 10px auto 10px;
+      #main,
+      #header,
+      #footer {
+        padding-left: 10px;
+        padding-right: 10px;
+      }
+    }
+  }
 </style>
