1a9bf3070a780284ea66904edc0c7f723e256a58
adashuai5 <ad873606339@gmail.com>
2019-01-14 23:15:04 +0800

    隐藏 log

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -22,7 +22,7 @@ export default function request(url,type = 'GET',data = {}){
             option.data = data
         }
         axios(option).then(res => {
-            console.log(res.data)
+            // console.log(res.data)
             if(res.data.status === 'ok'){
                 resolve(res.data)
             }else{
